e3522eb2e95e7062727cf83defcf32e8645195f1
Peter Szabo <pts@fazekas.hu>
2023-03-09 02:01:07 +0100

    added compilation scripts

---
+++ c-darwinc64.sh
@@ -0,0 +1,15 @@
+#! /bin/sh --
+# by pts@fazekas.hu at Thu Mar  9 01:11:17 CET 2023
+set -ex
+
+if type x86_64-apple-darwin14-gcc >/dev/null 2>&1; then
+  PREFIX=  # https://github.com/pts/pts-osxcross
+else
+  PREFIX="docker run -v --rm -it $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/"
+fi
+
+
+${PREFIX}x86_64-apple-darwin14-gcc -o mozjpegtran.darwinc64 -mmacosx-version-min=10.5 -Wl,-dead_strip -ffunction-sections -fdata-sections -lSystem -lcrt1.10.5.o -nostdlib -O2 -W -Wall -Wno-unused-variable -Wno-unused-parameter -Wno-sign-compare jpegtran.c rdswitch.c jcparam.c transupp.c jcext.c jerror.c cdjpeg.c jdapimin.c jcapimin.c jdatasrc.c jdtrans.c jctrans.c jdatadst.c jcicc.c jcomapi.c jutils.c jdmarker.c jmemmgr.c jdinput.c jcmarker.c jdhuff.c jdcoefct.c jdphuff.c jdarith.c jcmaster.c jchuff.c jcphuff.c jcarith.c jmemnobs.c jaricom.c jsimd_none.c
+${PREFIX}x86_64-apple-darwin14-strip  mozjpegtran.darwinc64
+
+: "$0" OK.
