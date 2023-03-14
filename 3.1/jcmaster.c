4de6cdc2b8e81bb0c624783369ae46528e091653
Peter Szabo <pts@fazekas.hu>
2023-03-14 18:39:27 +0100

    pacified gcc warning: this statement may fall through [-Wimplicit-fallthrough=]

--- 3.1/jcmaster.c
+++ 3.1/jcmaster.c
@@ -20,6 +20,7 @@
 #include "jinclude.h"
 #include "jpeglib.h"
 #include "jpegcomp.h"
+#include "jconfigint.h"
 #include "jmemsys.h"
 #include "jcmaster.h"
 
@@ -504,8 +505,8 @@ prepare_for_pass (j_compress_ptr cinfo)
      */
     master->pass_type = output_pass;
     master->pass_number++;
-    /*FALLTHROUGH*/
 #endif
+    FALLTHROUGH                 /*FALLTHROUGH*/
   case output_pass:
     /* Do a data-output pass. */
     /* We need not repeat per-scan setup if prior optimization pass did it. */
