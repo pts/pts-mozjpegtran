d0b51f15d3453eaa5fbddab7a71959282fb74126
Peter Szabo <pts@fazekas.hu>
2023-03-17 19:34:58 +0100

    added compilation scripts c-zigcclinux32.sh and c-zigcclinux64.sh

---
+++ 4.0.3/c-zigcclinux32.sh
@@ -0,0 +1,11 @@
+#! /bin/sh --
+# by pts@fazekas.hu at Thu Mar  9 01:11:17 CET 2023
+set -ex
+cd "${0%/*}"
+
+SRCS_4_0_3='jpegtran.c rdswitch.c cdjpeg.c jcparam.c transupp.c jcext.c jerror.c jdapimin.c jcapimin.c jdatasrc.c jdtrans.c jctrans.c jdatadst.c jcomapi.c jutils.c jdmarker.c jmemmgr.c jdinput.c jdphuff.c jdcoefct.c jdarith.c jdhuff.c jcmaster.c jchuff.c jcmarker.c jcphuff.c jmemnobs.c jcarith.c jaricom.c jcicc.c jsimd_none.c'
+# `zig cc' already does link-time optimizetion, e.g. removal of unused global functions.
+# Works with https://ziglang.org/download/0.10.1/zig-linux-x86_64-0.10.1.tar.xz
+zig cc -target i386-linux-musl -o mozjpegtran.zig32 -s -O2 -W -Wall -Wno-unused-parameter $SRCS_4_0_3 -lm
+
+: "$0" OK.
