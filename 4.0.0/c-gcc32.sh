050cafbc759c838cc1a24f0d6443111b0c176fe0
Peter Szabo <pts@fazekas.hu>
2023-03-17 17:52:41 +0100

    added compilation scripts for 4.0.0

---
+++ 4.0.0/c-gcc32.sh
@@ -0,0 +1,13 @@
+#! /bin/sh --
+# by pts@fazekas.hu at Thu Mar  9 01:11:17 CET 2023
+set -ex
+cd "${0%/*}"
+
+NOPIE=""  # GCC 4.8.4 doesn't have `-no-pie'.
+gcc -no-pie -E - </dev/null >/dev/null 2>&1 && NOPIE="-no-pie"
+
+SRCS_4_0_0='jpegtran.c rdswitch.c cdjpeg.c jcparam.c transupp.c jcext.c jerror.c jdapimin.c jcapimin.c jdatasrc.c jdtrans.c jctrans.c jdatadst.c jcomapi.c jutils.c jdmarker.c jmemmgr.c jdinput.c jdphuff.c jdcoefct.c jdarith.c jdhuff.c jcmaster.c jchuff.c jcmarker.c jcphuff.c jmemnobs.c jcarith.c jaricom.c jcicc.c jsimd_none.c'
+gcc -o mozjpegtran.gcc32 -m32 -march=i686 $NOPIE -s -O2 -Wl,--gc-sections -ffunction-sections -fdata-sections -W -Wall -Wno-unused-parameter $SRCS_4_0_0 -lm
+ls -ld mozjpegtran.gcc32
+
+: "$0" OK.
