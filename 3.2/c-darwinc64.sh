f46e5cc3de096d2a854e3008d70bb873885dfb97
Peter Szabo <pts@fazekas.hu>
2023-03-17 20:54:28 +0100

    added compilation scripts for 3.2

---
+++ 3.2/c-darwinc64.sh
@@ -0,0 +1,16 @@
+#! /bin/sh --
+# by pts@fazekas.hu at Thu Mar  9 01:11:17 CET 2023
+set -ex
+cd "${0%/*}"
+
+if type x86_64-apple-darwin14-gcc >/dev/null 2>&1; then
+  PREFIX=  # https://github.com/pts/pts-osxcross
+else
+  PREFIX="docker run -v --rm -it $PWD:/workdir multiarch/crossbuild /usr/osxcross/bin/"
+fi
+
+SRCS_3_2='jpegtran.c rdswitch.c cdjpeg.c jcparam.c transupp.c jcext.c jerror.c jdapimin.c jcapimin.c jdatasrc.c jdtrans.c jctrans.c jdatadst.c jcomapi.c jutils.c jdmarker.c jmemmgr.c jdinput.c jdphuff.c jdcoefct.c jdarith.c jdhuff.c jcmaster.c jchuff.c jcmarker.c jcphuff.c jmemnobs.c jcarith.c jaricom.c jsimd_none.c'
+${PREFIX}x86_64-apple-darwin14-gcc -o mozjpegtran.darwinc64 -mmacosx-version-min=10.5 -Wl,-dead_strip -ffunction-sections -fdata-sections -lSystem -lcrt1.10.5.o -nostdlib -O2 -W -Wall -Wno-unused-parameter $SRCS_3_2
+${PREFIX}x86_64-apple-darwin14-strip  mozjpegtran.darwinc64
+
+: "$0" OK.
---
+++ 3.2/c-owccwin32.sh
@@ -0,0 +1,11 @@
+#! /bin/sh --
+# by pts@fazekas.hu at Thu Mar  9 01:11:17 CET 2023
+set -ex
+cd "${0%/*}"
+
+SRCS_3_2='jpegtran.c rdswitch.c cdjpeg.c jcparam.c transupp.c jcext.c jerror.c jdapimin.c jcapimin.c jdatasrc.c jdtrans.c jctrans.c jdatadst.c jcomapi.c jutils.c jdmarker.c jmemmgr.c jdinput.c jdphuff.c jdcoefct.c jdarith.c jdhuff.c jcmaster.c jchuff.c jcmarker.c jcphuff.c jmemnobs.c jcarith.c jaricom.c jsimd_none.c'
+owcc -o mozjpegtran.ow.exe -bwin32 -fo=.oo -s -Os -fno-stack-check -fsigned-char -march=i386 -Wl,option -Wl,eliminate -Wl,runtime -Wl,console=3.10 -W -Wall -Wextra -Wno-n202 -Wno-n303 -Wno-n136 -Wno-n201 $SRCS_3_2 nouser32.c
+rm -f ./*.oo
+ls -ld mozjpegtran.ow.exe
+
+: "$0" OK.
